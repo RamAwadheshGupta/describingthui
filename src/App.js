99770d030b0749f633197629d4f3c5609a9df411
Ram <guptaram94@gmail.com>
2023-03-02 13:22:35 +0530

    crate todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
+import Profile from './components/Profile';
+import TodoList from './components/TodoList';
 
-function App() {
+function Profiles()
+{
+  return (
+    <img
+      src='https://i.imgur.com/MK3eW3As.jpg'
+      alt='Johnson katherine'
+    />
+  );
+}
+
+function Gallery()
+{
+  return (
+    <>
+      <h2>Profile User</h2>
+      <Profiles />
+      <Profiles />
+      <Profiles />
+      <br />
+      <h2>importing and exporting components</h2>
+      <Profile />
+      <Profile />
+      <Profile />
+      <TodoList />
+    </>
+  );
+}
+
+function App()
+{
   return (
     <div className="App">
-      
+      <section>
+        <Gallery />
+      </section>
     </div>
   );
 }
 
 export default App;
+
+
