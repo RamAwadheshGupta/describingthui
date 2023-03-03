694b9fe321963a4594e0c844e996d7480c88f886
Ram <guptaram94@gmail.com>
2023-03-03 17:52:53 +0530

    Rendering  list processing

---
+++ src/components/RenderingLists.js
@@ -0,0 +1,12 @@
+import { people } from './data.js'
+function RenderingLists()
+{
+
+    return (
+        <article>
+            <h1>Scientists</h1>
+        </article>
+    );
+}
+
+export default RenderingLists;
